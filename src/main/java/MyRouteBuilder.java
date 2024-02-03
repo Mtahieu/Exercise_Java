eebb0bfac7b6f2b92417d629ead2ec0d1e2e3a29
Mtahieu <mathieutei@yahoo.de>
2024-02-03 09:38:02 +0100

    created JSON and lazy workaround for jsonpath

--- src/main/java/MyRouteBuilder.java
+++ src/main/java/MyRouteBuilder.java
@@ -12,11 +12,11 @@ public class MyRouteBuilder extends RouteBuilder {
      */
     public void configure() {
 
-        // here is a sample which processes the input files
-        // (leaving them in place - see the 'noop' flag)
-        // then performs content based routing on the message using XPath
-        from("file:src/data?noop=true")
+        // Only .xml files here
+        from("file:src/data?noop=true&antInclude=*.xml") //&antInclude=**/*.xml$
             .choice()
+
+                //London Handling
                 .when(xpath("/person/city = 'London'")).process(new Processor() {
             public void process(Exchange exchange) throws Exception {
                 // DRAWBACK: HEADER ETC GETS LOST
@@ -26,12 +26,21 @@ public class MyRouteBuilder extends RouteBuilder {
         })
                     .log("UK message")
                     .to("file:target/messages/uk?fileName=uk.txt")
-                .when(xpath("/person/city = 'Karlsruhe'"))
-                .log("DE message")
-                .to("file:target/messages/de")
-                .otherwise()
+
+                // Karlsruhe handling
+                    .when(xpath("/person/city = 'Karlsruhe'"))
+                    .log("DE message")
+                    .to("file:target/messages/de")
+                    .otherwise()
                     .log("Other message")
                     .to("file:target/messages/others");
+
+        from("file:src/data?noop=true&antInclude=*.json").choice()
+                .when().simple("${bodyAs(String)} contains 'Karlsruhe'").to("file:target/messages/JSON/de")
+
+                .when().simple("${bodyAs(String)} contains 'London'").to("file:target/messages/JSON/uk")
+
+                .otherwise().to("file:target/messages/JSON/other");
     }
 
 }
